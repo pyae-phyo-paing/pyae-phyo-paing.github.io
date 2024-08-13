00e216a79133490a579373fca9499686ac55a866
pyae-phyo-paing <paing.mdy.edu@gmail.com>
2024-08-13 21:50:37 +0630

    August13.2024.jQueryLessons

--- jQuery/TechShop/scripts.js
+++ jQuery/TechShop/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
+    
+    
+    getData();
+    count();
 
+
+    
     $('.addToCart').click(function(){
         // alert("Hello");
         let id = $(this).data('id');
         let name = $(this).data('name');
         let price = $(this).data('price');
-        console.log(id,name,price);
+        // console.log(id,name,price);
 
         let items = {
             id : id,
@@ -13,7 +19,6 @@ $(document).ready(function(){
             price : price,
             qty : 1
         }
-
         let itemsString = localStorage.getItem('shops');
         let itemsArray ;
         if (itemsString == null) {
@@ -21,7 +26,6 @@ $(document).ready(function(){
         }else{
             itemsArray = JSON.parse(itemsString);
         }
-
         let status = false;  // သူတို့က true and false နှစ်ခုပဲ ရှိတာ 
         $.each(itemsArray, function(i,v){
             if(v.id == id){ 
@@ -37,7 +41,56 @@ $(document).ready(function(){
 
         let itemsData = JSON.stringify(itemsArray);
         localStorage.setItem('shops', itemsData);
-        
+
+        count();
     })
 
+
+
+    function getData(){
+        let dataString = localStorage.getItem('shops');
+        if (dataString){
+            let dataArray = JSON.parse(dataString);
+            // console.log(dataArray);
+            
+
+            let table = '';
+            let j = 1;
+            let totalPrice = 0;
+
+            $.each(dataArray,function(i,v){
+
+                table += `<tr>
+                            <td>${j++}</td>
+                            <td>${v.name}</td>
+                            <td>${v.price}</td>
+                            <td><button style="margin-right:10px; padding:5px 5px 5px; font-size:20px;">+</button>${v.qty}<button style="margin-left:10px; padding:5px 5px 5px;font-size:20px;">-</button></td>
+                            <td>${v.price * v.qty}</td>
+                          </tr>`
+                
+                totalPrice += v.price * v.qty;
+
+            })
+
+            table += `<tr>
+                        <th colspan="4">Total</th>
+                        <td>${totalPrice}</td>
+                      </tr>`
+
+            $('#mytable').html(table);
+        }
+    }
+
+
+
+    function count() {
+        let itemString = localStorage.getItem('shops');
+        if (itemString) {
+            let itemsArray = JSON.parse(itemString);
+            if (itemsArray != 0){  //index.html မှာ cart noti မှာ 0 ပေးထားတဲ့အတွက်ကြောင့် 
+                let count = itemsArray.length;
+                $('.nav_home2_text').text(count);
+            }
+        }
+    }
 })
